dc010c0509cc8502d7eb6e8e674a6f9583b167a0
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-03-30 23:25:10 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Page</title>
+  </head>
+  <body>
+    <h1>This is a HTML header!</h1>
+    <?php
+      echo "This is some php text!";
+    ?>
+  </body>
+</html>
